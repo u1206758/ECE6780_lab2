9e37d6a2e8895aa4f2a6fe6f8d59c058a1decefc
Adam Blakesee <adam@Adams-MacBook-Pro-3.local>
2024-02-07 15:07:08 -0700

    Lab complete

---
+++ lab2-part1/Src/main.c
@@ -0,0 +1,243 @@
+/**
+  *
+  *
+  *
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f0xx_hal.h"
+void _Error_Handler(char * file, int line);
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+//EXTI0 Interrupt Handler
+void EXTI0_1_IRQHandler(void)
+{
+  //Toggle green and orange LEDs
+  GPIOC->ODR ^= GPIO_ODR_8;
+  GPIOC->ODR ^= GPIO_ODR_9;
+  //Clear EXTI pending flag
+  EXTI->PR |= EXTI_PR_PR0;
+}
+
+//Initialize all four LEDs
+void init_leds(void)
+{
+  //Initialize red LED, PC6
+  GPIOC->MODER |= GPIO_MODER_MODER6_0; //General purpose output
+  GPIOC->OTYPER &= ~GPIO_OTYPER_OT_6; // Push-pull
+  GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEEDR6_0; //Low speed
+  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR6_0 | GPIO_PUPDR_PUPDR6_1); //No pull up or down
+
+  //Initialize blue LED, PC7
+  GPIOC->MODER |= GPIO_MODER_MODER7_0; //General purpose output
+  GPIOC->OTYPER &= ~GPIO_OTYPER_OT_7; // Push-pull
+  GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEEDR7_0; //Low speed
+  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR7_0 | GPIO_PUPDR_PUPDR7_1); //No pull up or down
+
+  //Initialize orange LED, PC8
+  GPIOC->MODER |= GPIO_MODER_MODER8_0; //General purpose output
+  GPIOC->OTYPER &= ~GPIO_OTYPER_OT_8; // Push-pull
+  GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEEDR8_0; //Low speed
+  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR8_0 | GPIO_PUPDR_PUPDR8_1); //No pull up or down
+
+  //Initialize green LED, PC9
+  GPIOC->MODER |= GPIO_MODER_MODER9_0; //General purpose output
+  GPIOC->OTYPER &= ~GPIO_OTYPER_OT_9; // Push-pull
+  GPIOC->OSPEEDR &= ~GPIO_OSPEEDR_OSPEEDR9_0; //Low speed
+  GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR9_1); //No pull up or down
+
+}
+
+int main(void) 
+{
+  HAL_Init(); // Reset of all peripherals, init the Flash and Systick
+  SystemClock_Config(); //Configure the system clock
+
+  //Enable GPIOC and GPIOA clock
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN; 
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+  
+  //Initialize all LEDs
+  init_leds(); 
+  
+  //Set green LED on
+  GPIOC->BSRR |= GPIO_BSRR_BS_9;
+
+  //Initialize button
+  GPIOA->MODER &= ~(GPIO_MODER_MODER0 | GPIO_MODER_MODER1); //Input
+  GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEEDR0; //Low speed
+  GPIOA->PUPDR |= GPIO_PUPDR_PUPDR0_1; //Pull down
+  GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR0_0; //Pull down
+
+  //Unmask EXTI0
+  EXTI->IMR |= EXTI_IMR_MR0;
+  //EXTI0 rising edge trigger
+  EXTI->RTSR |= EXTI_RTSR_TR0;
+  //Enable RCC for SYSCFG
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+  //Route PA0 to EXTI0
+  SYSCFG->EXTICR[1] &= ~(SYSCFG_EXTICR1_EXTI0_PA);
+  //Enable EXTI0 interrupt
+  NVIC_EnableIRQ(EXTI0_1_IRQn);
+  //Set EXTI0 interrupt priority to 1
+  NVIC_SetPriority(EXTI0_1_IRQn, 1);
+
+  while (1) 
+  {
+    //Delay 
+    HAL_Delay(500); 
+    //Toggle red LED
+    GPIOC->ODR ^= GPIO_ODR_6;
+  }
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void _Error_Handler(char * file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+*/
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
